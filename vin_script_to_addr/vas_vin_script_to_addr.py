3abf9114c50db1503ba4b2ff299d522b7865b569
mutalisk <tangjian@blocklinker.com>
2019-11-13 17:32:17 +0800

    add vas support

---
+++ vin_script_to_addr/vas_vin_script_to_addr.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import base58
+import utils
+
+
+OP_0 = chr(0)
+
+ADDR_PREFIX = chr(70)
+SCRIPT_PREFIX = chr(5)
+
+# no testnet, so use the same prefix as mainnet
+ADDR_PREFIX_TEST = chr(70)
+SCRIPT_PREFIX_TEST = chr(5)
+
+
+# p2pkh
+def pubkey_to_legacy_address(pubkey, main_net=True):
+    pubkey = pubkey.decode("hex")
+
+    addr_prefix = ADDR_PREFIX
+    if not main_net:
+        addr_prefix = ADDR_PREFIX_TEST
+
+    h20 = utils.hash160(pubkey)
+    d21 = addr_prefix + h20
+    c4 = utils.checksum(d21)
+
+    d25 = d21 + c4
+    return base58.b58encode(d25)
+
+
+# p2sh
+def redeemscript_to_p2sh_address(redeem_script, main_net=True):
+    redeem_script = redeem_script.decode("hex")
+
+    script_prefix = SCRIPT_PREFIX
+    if not main_net:
+        script_prefix = SCRIPT_PREFIX_TEST
+
+    h20 = utils.hash160(redeem_script)
+    d21 = script_prefix + h20
+    c4 = utils.checksum(d21)
+
+    d25 = d21 + c4
+    return base58.b58encode(d25)
+
+
+def func_test():
+    # txid:
+    #
+    print pubkey_to_legacy_address("")
+
+    # txid:
+    #
+    print redeemscript_to_p2sh_address("")
+
+
+if __name__ == "__main__":
+    func_test()
+
